a1b8e76430dbb44f3182e4ad72ca0cfdf8ed4a29
Arthur25 <98665507+Arthur2500@users.noreply.github.com>
2024-08-20 21:08:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ const helmet = require('helmet');
 const app = express();
 const port = 3000;
 
-app.use(helmet()); // Adds security headers to the app using Helmet
+app.use(helmet({
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+    crossOriginResourcePolicy: false,
+}));
 
 // Set up multer for file uploads with file type validation and size limit
 const upload = multer({ 
